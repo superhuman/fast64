9a317afb54ce82a4aa990b237a4fae403da8dc12
Bhavesh Kakadiya <bhavesh@superhuman.com>
2018-11-11 18:35:38 -0800

    Make the urlencode even faster by introducing it's very own modified lookup table.

--- index.js
+++ index.js
@@ -24,36 +24,47 @@
  */
 var B64 = {
   alphabet: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=',
-  // rlookup was created using B64.buildRlookup()
+  // rlookup was created using B64.buildRlookup(false)
   rlookup: [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 43, 47, 61],
-  // lookup was created using B64.buildLookup()
+  // rlookup was created using B64.buildRlookup(true)
+  urlrlookup: [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 45, 95, 61],
+  // lookup was created using B64.buildLookup
   lookup: [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, 62, null, 62, null, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, null, null, null, 64, null, null, null, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, null, null, null, null, 63, null, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, null, null],
-  encode: function (s) {
+
+  encode: function (s, options) {
     var buffer = typeof s === 'string' ? B64.toUtf8(s) : s
     var position = -1
-    var result
     var len
+    var result
+    var rlookup
     var nan0, nan1, nan2
 
     len = buffer.length
 
     result = new Uint8Array(new ArrayBuffer(Math.ceil(len / 3) * 4))
-    var i = 0
+    rlookup = B64.rlookup
+    if (options && options.url) {
+      // spot the subtle difference :)
+      result = new Uint8Array(new ArrayBuffer(Math.ceil(4 * len / 3)))
+      rlookup = B64.urlrlookup
+    }
 
+    var i = 0
     while (++position < len) {
       nan0 = buffer[position]
       nan1 = buffer[++position]
-      result[i++] = B64.rlookup[nan0 >> 2]
-      result[i++] = B64.rlookup[((nan0 & 3) << 4) | (nan1 >> 4)]
+      result[i++] = rlookup[nan0 >> 2]
+      result[i++] = rlookup[((nan0 & 3) << 4) | (nan1 >> 4)]
       if (isNaN(nan1)) {
-        result[i++] = B64.rlookup[64]
-        result[i++] = B64.rlookup[64]
+        result[i++] = rlookup[64]
+        result[i++] = rlookup[64]
       } else {
         nan2 = buffer[++position]
-        result[i++] = B64.rlookup[((nan1 & 15) << 2) | (nan2 >> 6)]
-        result[i++] = B64.rlookup[(isNaN(nan2)) ? 64 : nan2 & 63]
+        result[i++] = rlookup[((nan1 & 15) << 2) | (nan2 >> 6)]
+        result[i++] = rlookup[(isNaN(nan2)) ? 64 : nan2 & 63]
       }
     }
+
     return new TextDecoder('utf8').decode(result)
   },
   decode: function (s, options) {
@@ -64,18 +75,22 @@ var B64 = {
     if (/[^A-Za-z0-9+\/\-_=]/g.test(s)) { // eslint-disable-line
       throw new Error('InvalidCharacterError: decode failed: The string contains characters invalid in a base64 encoded string.')
     }
-
     return B64.decodeCommon(s, options)
   },
   toUtf8: function (s) {
     return new TextEncoder('utf8').encode(s)
   },
-  buildRlookup: function () {
+  buildRlookup: function (url) {
     if (!B64.rlookup) {
       var position = -1
       var len = B64.alphabet.length
       B64.rlookup = new Array(125)
       while (++position < len) { B64.rlookup[position] = B64.alphabet.charCodeAt(position) }
+      if (url) {
+        B64.urlrlookup = Object.clone(B64.rlookup, true)
+        B64.urlrlookup[62] = '-'.charCodeAt(0)
+        B64.urlrlookup[63] = '_'.charCodeAt(0)
+      }
     }
   },
   buildLookup: function () {
@@ -83,9 +98,10 @@ var B64 = {
       var position = -1
       var len = B64.alphabet.length
       B64.lookup = new Array(125)
+      while (++position < len) { B64.lookup[B64.alphabet.charCodeAt(position)] = position }
+      // we dont need to special case these as they are not overlapping
       B64.lookup['-'.charCodeAt(0)] = 62
       B64.lookup['_'.charCodeAt(0)] = 63
-      while (++position < len) { B64.lookup[B64.alphabet.charCodeAt(position)] = position }
     }
   },
   decodeCommon: function (s, options) {
@@ -138,11 +154,7 @@ var B64url = {
   },
 
   encode: function (input) {
-    var output = B64.encode(input)
-    return output
-      .replace(/\+/g, '-')
-      .replace(/\//g, '_')
-      .split('=', 1)[0]
+    return B64.encode(input, {url: true})
   }
 }
 
