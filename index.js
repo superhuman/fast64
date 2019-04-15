4cf4517b354fbc98838091b94d2b684170d7174d
Bhavesh Kakadiya <bhavesh@superhuman.com>
2019-04-14 23:54:19 -0700

    Build lookups in a LookupFactory to reduce the download size of the library.

--- index.js
+++ index.js
@@ -24,11 +24,11 @@
  */
 var B64 = {
   alphabet: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=',
-  // rlookup was created using B64.buildRlookup(false)
+  // rlookup was created using LookupFactory.buildRlookup(false)
   rlookup: [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 43, 47, 61],
-  // rlookup was created using B64.buildRlookup(true)
+  // rlookup was created using LookupFactory.buildRlookup(true)
   urlrlookup: [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 45, 95, 61],
-  // lookup was created using B64.buildLookup
+  // lookup was created using LookupFactory.buildLookup()
   lookup: [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, 62, null, 62, null, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, null, null, null, 64, null, null, null, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, null, null, null, null, 63, null, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, null, null],
 
   encode: function (s, options) {
@@ -67,6 +67,7 @@ var B64 = {
 
     return new TextDecoder('utf8').decode(result)
   },
+
   decode: function (s, options) {
     s = s.replace(/\s/g, '')
     if (s.length % 4) {
@@ -77,33 +78,11 @@ var B64 = {
     }
     return B64.decodeCommon(s, options)
   },
+
   toUtf8: function (s) {
     return new TextEncoder('utf8').encode(s)
   },
-  buildRlookup: function (url) {
-    if (!B64.rlookup) {
-      var position = -1
-      var len = B64.alphabet.length
-      B64.rlookup = new Array(125)
-      while (++position < len) { B64.rlookup[position] = B64.alphabet.charCodeAt(position) }
-      if (url) {
-        B64.urlrlookup = Object.clone(B64.rlookup, true)
-        B64.urlrlookup[62] = '-'.charCodeAt(0)
-        B64.urlrlookup[63] = '_'.charCodeAt(0)
-      }
-    }
-  },
-  buildLookup: function () {
-    if (!B64.lookup) {
-      var position = -1
-      var len = B64.alphabet.length
-      B64.lookup = new Array(125)
-      while (++position < len) { B64.lookup[B64.alphabet.charCodeAt(position)] = position }
-      // we dont need to special case these as they are not overlapping
-      B64.lookup['-'.charCodeAt(0)] = 62
-      B64.lookup['_'.charCodeAt(0)] = 63
-    }
-  },
+
   decodeCommon: function (s, options) {
     var position = -1
     var array = new ArrayBuffer(s.length / 4 * 3)
